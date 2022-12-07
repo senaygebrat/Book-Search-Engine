edfceaa197b2461471a280e8f4a8afc31bc235d9
Senay Gebrat <senaygebrat@gmail.com>
2022-12-06 19:32:26 -0800

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+// const db = require('./config/connection'); 
+// const routes = require('./routes');
+
+//remove db and routes variables when switching
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +21,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+})
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
