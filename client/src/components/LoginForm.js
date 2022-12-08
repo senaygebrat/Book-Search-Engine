797256011118d93ba50dfbc8e5b9a0f462b6499e
Senay Gebrat <senaygebrat@gmail.com>
2022-12-08 13:01:49 -0800

    modified file structure and loginform

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,7 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/react-hooks';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -8,6 +9,7 @@ import Auth from '../utils/auth';
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
+  const [login, { error }] = useMutation(LOGIN_USER);
   const [showAlert, setShowAlert] = useState(false);
 
   const handleInputChange = (event) => {
@@ -26,26 +28,21 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      const { data } = await login({ 
+        variables: { ...userFormData },
+      });
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(error);
+      setShowAlert(true);
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
+      setUserFormData({
+        username: "",
+        email: "",
+        password: "",
+      })
+    };
 
   return (
     <>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-// const db = require('./config/connection'); 
+const db = require('./config/connection'); 
 // const routes = require('./routes');
 
-//remove db and routes variables when switching
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -27,8 +29,16 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// })
+// };
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
 })
 };
 
